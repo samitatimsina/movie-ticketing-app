ead6ee05d16e18d109d417ad6000ace3562ebc48
Samita Timsina <samitatimsina910@gmail.com>
2026-06-12 12:35:35 +0545

    minor bug fixes

--- bms-frontend/vite.config.js
+++ bms-frontend/vite.config.js
@@ -1,17 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),  tailwindcss()],
-server: {
-    proxy: {
-      "/api": import.meta.env.VITE_BACKEND_URL
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+        },
+      },
     },
-  },
-  build: {
-    sourcemap: false,
-    minify: "esbuild"
-  }
+    build: {
+      sourcemap: false,
+      minify: "esbuild",
+    },
+  };
 });
